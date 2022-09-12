b016794b2aaf9e775820a16625275ef9d1711751
INGJEISSON <30418444+INGJEISSON@users.noreply.github.com>
2022-09-11 20:11:53 -0500

    maps-locations

--- Lib/Db.js
+++ Lib/Db.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  //baseURL: `https://api.avill.com.co/api/`,
- baseURL: `http://192.168.1.1:8000/api/`,
+  baseURL: `https://api.avill.com.co/api/`,
+ //baseURL: `http://192.168.1.1:8000/api/`,
 });
